cb35f452d9cd6e2b2eba8e7a6dfee9983749031b
Abdelzaher <oabdelzaher68@gmail.com>
2020-09-18 17:54:19 +0200

    login

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,10 @@ class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              height: 350,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
                   bottomRight: Radius.circular(25.0),
@@ -74,118 +72,127 @@ class _LogInState extends State<LogIn> {
                 ),
               ),
             ),
-            SizedBox(height: 5,),
-            //username
-            Padding(
-              padding: const EdgeInsets.only(
-                right: 10,
-                left: 10,
-                top: 10,
-              ),
-              child: Container(
-                height: 45,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.grey[100],
-                ),
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Username',
-                      icon: Icon(
-                        Icons.person_outline,
-                        color: Colors.pink[600],
-                      ),
-                      border: InputBorder.none,
-
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            //password
-            Padding(
-              padding: const EdgeInsets.only(
-                right: 10,
-                left: 10,
-                top: 10,
-
-              ),
-              child: Container(
-                height: 45,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.grey[100],
-                ),
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      icon: Icon(
-                        Icons.lock_outline,
-                        color: Colors.pink[600],
-                      ),
-                      border: InputBorder.none,
-
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            //forget password
-            Padding(
-              padding: const EdgeInsets.only(
-                  bottom: 5,
-                top: 5,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Forget Password',
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                      fontWeight: FontWeight.w400,
-                      fontStyle: FontStyle.italic,
-                      fontSize: 11.0,
-                    ),
-                  ),
-
-                ],
-              ),
-            ),
-            //login button
-            Row(
+          ),
+          //SizedBox(height: 5,),
+          //username
+          Expanded(
+            child: Column(
               children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      right: 45,
-                      left: 45,
-                      bottom: 3,
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 10,
+                    left: 10,
+                    top: 10,
+                  ),
+                  child: Container(
+                    height: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.grey[100],
                     ),
-                    child: Container(
-                      height: 35,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: Colors.pink[600],
-                      ),
-                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                      child: FlatButton(
-                          onPressed: (){},
-                          child: Text('Login'),
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Username',
+                          icon: Icon(
+                            Icons.person_outline,
+                            color: Colors.pink[600],
+                          ),
+                          border: InputBorder.none,
+
+                        ),
                       ),
                     ),
                   ),
                 ),
+                //password
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 10,
+                    left: 10,
+                    top: 10,
+
+                  ),
+                  child: Container(
+                    height: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.grey[100],
+                    ),
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          icon: Icon(
+                            Icons.lock_outline,
+                            color: Colors.pink[600],
+                          ),
+                          border: InputBorder.none,
+
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5,),
+                //forget password
+                Padding(
+                  padding: const EdgeInsets.only(
+                      bottom: 5,
+                    top: 5,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Forget Password',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 11.0,
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+                Spacer(flex: 1,),
+                //login button
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          right: 45,
+                          left: 45,
+                          bottom: 3,
+                        ),
+                        child: Container(
+                          height: 35,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.pink[600],
+                          ),
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          child: FlatButton(
+                              onPressed: (){},
+                              child: Text('Login'),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5,),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
